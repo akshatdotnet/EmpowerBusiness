7a623e602350291196c56f1f2ed0f35546f6023f
akshatdotnet <akshatdotnet@outlook.com>
2024-08-17 13:26:56 +0530

    Commit message

---
+++ EmpowerBusiness/Empower.BMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Empower.BMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
